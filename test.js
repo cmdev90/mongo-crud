3ce1f3b2a24e8c3d3c0adbda6930001bd05db6e8
wryi <cherlton.millette@gmail.com>
2016-05-26 00:42:28 -0400

    Clean up some old testing code...

--- test.js
+++ test.js
@@ -1,10 +1,8 @@
-var Mongo = require('./')
-  , async = require('async');
-
+var Mongo = require('./');
 var assert = require('chai').assert;
 
 var db = 'mongodb://localhost/triniclassifieds'
-  , alias = 'Classifieds';
+var alias = 'Classifieds';
 
 var person = {
   name: 'Kalidia', 
@@ -97,47 +95,3 @@ describe('Mongo', function (){
     });
   });
 });
-
-
-
-
-
-// describe('Array', function() {
-//   describe('#indexOf()', function () {
-//     it('should return -1 when the value is not present', function () {
-//       assert.equal(-1, [1,2,3].indexOf(5));
-//       assert.equal(-1, [1,2,3].indexOf(0));
-//     });
-//   });
-// });
-
-// console.log(mongo);
-
-// mongo.connect('mongodb://localhost/db');
-
-// // wait 2 seconds to confirm the connection and initilize the object.
-// setTimeout(function () {
-
-// 	if (mongo.isReady()) { 
-
-// 		mongo.create('People', {name: 'Kalidia Millette', position: 'Cow Mechanic', rank: '...'}, function (err, result){
-//       console.log(err, result); 
-//       var recent = result.insertedIds[0];
-//       mongo.retrieve('People', {name: 'Kalidia Millette'}, function (err, result) {
-//         console.log(err, result);
-//         mongo.update('People', {_id: recent, rank: 'Novice'} ,function (err, result){
-//           console.log(err, result);
-//           mongo.retrieve('People', {name: 'Kalidia Millette'}, function (err, result) {
-//             console.log(err, result);
-//             mongo.delete('People', function (err, result) {
-//               console.log(err, result);
-//               mongo.retrieve('People', {name: 'Kalidia Millette'}, function (err, result) {
-//                 console.log(err, result);
-//               });
-//             })
-//           });
-//         })
-//       });
-//   	});
-// 	}
-// }, 2000);
